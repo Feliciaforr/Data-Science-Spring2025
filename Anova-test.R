ea092dbc4514e38e756232dceec96842fd6680a0
Feliciaforr <124526337+Feliciaforr@users.noreply.github.com>
2025-03-03 12:33:46 -0500

    Add files via upload

--- Anova-test.R
+++ Anova-test.R
@@ -16,8 +16,15 @@ levels(factor(df$treat))
 #Optional: Plot data
 ggplot(df, aes(x = poison, y = time, fill = poison)) + geom_boxplot() + geom_jitter(shape = 15, color = "steelblue", position = position_jitter(0.21)) + theme_classic()
 
-#Anova Test
+#Applying Anova Test Function
 anova_one_way <- aov(time~poison, data = df) 
 
 #Summary of Anova Test
 summary(anova_one_way)
+
+ Df Sum Sq Mean Sq F value   Pr(>F)    
+poison       1 0.9316  0.9316   20.67 3.96e-05 ***
+Residuals   46 2.0735  0.0451                     
+---
+Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
